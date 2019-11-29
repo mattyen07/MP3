cc0f3e7498ee31ac349b39e76d96205fb872d2b2
Admin <beno3313@gmail.com>
2019-11-28 19:56:05 -0800

    expiryThread mostly complete

--- src/main/java/cpen221/mp3/cache/ExpiryThread.java
+++ src/main/java/cpen221/mp3/cache/ExpiryThread.java
@@ -7,11 +7,28 @@ import java.time.LocalDateTime;
 public class ExpiryThread<T extends Cacheable> implements Runnable {
     private final Map<T, TimePair> cacheMap;
 
-    public ExpiryThread(Map<T, TimePair> map) {
-        this.cacheMap = map;
+    /*
+    RI: cacheMap objects with accurate TimePairs stored in cacheObjects.
+        this.cacheMap is not null.
+     */
 
+    /*
+    AF:
+     */
+
+    /**
+     * Initializes new instance of ExpiryThread with a map of the objects in the cache.
+     * @param cacheObjects is not null and its keys are the objects in the cache and the values are the TimePair's
+     *            corresponding to the objects.
+     */
+    public ExpiryThread(Map<T, TimePair> cacheObjects) {
+        this.cacheMap = cacheObjects;
     }
 
+    /**
+     * Runs ExpiryThread. This constantly updates the cache by removing stale (expired) objects.
+     * @modifies cacheMap therefore modifying the cache by removing stale objects.
+     */
     public void run() {
         while (true) {
             Set<T> copyMap = this.cacheMap.keySet();
