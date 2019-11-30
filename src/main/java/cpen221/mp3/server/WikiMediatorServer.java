6f3803e4c9ab49d1fe707857b73e7a2af45a7997
Admin <beno3313@gmail.com>
2019-11-29 16:58:22 -0800

    working on server

--- src/main/java/cpen221/mp3/server/WikiMediatorServer.java
+++ src/main/java/cpen221/mp3/server/WikiMediatorServer.java
@@ -24,8 +24,9 @@ public class WikiMediatorServer {
      * Start a server at a given port number, with the ability to process
      * upto n requests concurrently.
      *
-     * @param port the port number to bind the server to
-     * @param n the number of concurrent requests the server can handle
+     * @param port the port number to bind the server to.
+     *           0 <= port <= 65535
+     * @param n > 0 the number of concurrent requests the server can handle
      */
 
     public WikiMediatorServer(int port, int n) {
