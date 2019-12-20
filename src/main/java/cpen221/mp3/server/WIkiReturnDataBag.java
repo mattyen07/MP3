99e7048518022a1637c4a7e8e4d7344619125861
Admin <beno3313@gmail.com>
2019-12-20 08:48:21 -0800

    working on server and client

---
+++ src/main/java/cpen221/mp3/server/WIkiReturnDataBag.java
@@ -0,0 +1,7 @@
+package cpen221.mp3.server;
+
+public class WIkiReturnDataBag {
+    String id = null;
+    String  status = null;
+
+}
--- src/main/java/cpen221/mp3/server/WikiMediatorClient.java
+++ src/main/java/cpen221/mp3/server/WikiMediatorClient.java
@@ -11,21 +11,96 @@ import java.net.UnknownHostException;
 import com.google.gson.*;
 
 //https://docs.oracle.com/javase/tutorial/networking/sockets/examples/EchoClient.java
+//https://www.baeldung.com/a-guide-to-java-sockets
 
 public class WikiMediatorClient {
-    private static String hostName = "localHost";
+    private Socket socket;
+    private BufferedReader in;
+    private PrintWriter out;
+    // Rep invariant: socket, in, out != null
 
-    public WikiMediatorClient(int portNumber) {
-        try(
-                Socket socket = new Socket(InetAddress.getLocalHost(), 42069);
-                ) {
+    /**
+     * Make a FibonacciClient and connect it to a server running on
+     * hostname at the specified port.
+     * @throws IOException if can't connect
+     */
+    public FibonacciClient(String hostname, int port) throws IOException {
+        socket = new Socket(hostname, port);
+        in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        out = new PrintWriter(new OutputStreamWriter(socket.getOutputStream()));
+    }
 
-        } catch(UnknownHostException e) {
-            System.out.println("Unknown Host Exception");
-        } catch(java.io.IOException e) {
-            System.out.println("Couldn't get I/O for the connection to " + hostName);
+    /**
+     * Send a request to the server. Requires this is "open".
+     * @param request is a correctly formatted JsonObject
+     * @throws IOException if network or server failure
+     */
+    public void sendRequest(JsonObject request) throws IOException {
+        out.print(request);
+        out.flush(); // important! make sure x actually gets sent
+    }
+
+    /**
+     * Get a reply from the next request that was submitted.
+     * Requires this is "open".
+     * @return the requested Fibonacci number
+     * @throws IOException if network or server failure
+     */
+    public JsonObject getReply() throws IOException {
+        String reply = in.readLine();
+        if (reply == null) {
+            throw new IOException("connection terminated unexpectedly");
+        }
+
+        try {
+            return new JsonObject(reply);
+        } catch (NumberFormatException nfe) {
+            throw new IOException("misformatted reply: " + reply);
         }
     }
+
+    /**
+     * Closes the client's connection to the server.
+     * This client is now "closed". Requires this is "open".
+     * @throws IOException if close fails
+     */
+    public void close() throws IOException {
+        in.close();
+        out.close();
+        socket.close();
+    }
+
+
+
+
+    private static final int N = 100;
+
+    /**
+     * Use a FibonacciServer to find the first N Fibonacci numbers.
+     */
+    public static void main(String[] args) {
+        try {
+            FibonacciClient client = new FibonacciClient("localhost", FibonacciServer.FIBONACCI_PORT);
+
+            // send the requests to find the first N Fibonacci numbers
+            for (int x = 1; x <= N; ++x) {
+                client.sendRequest(x);
+                System.out.println("fibonacci("+x+") = ?");
+            }
+
+            // collect the replies
+            for (int x = 1; x <= N; ++x) {
+                BigInteger y = client.getReply();
+                System.out.println("fibonacci("+x+") = "+y);
+            }
+
+            client.close();
+        } catch (IOException ioe) {
+            ioe.printStackTrace();
+        }
+    }
+
+
 
 
 
